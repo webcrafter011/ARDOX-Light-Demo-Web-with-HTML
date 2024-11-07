902ce62a6fad21ac3bad83f984d6f14b1b32dcac
fuhad-shiblu <fuhadshiblu.web@gmail.com>
2024-11-07 14:15:31 +0600

    Light Mode

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//========== DOM Variables
+let button                  = document.querySelector('.button');
+let fuhadShiblu_inputPass   = document.querySelector('.fuhadShiblu_inputPass');
+let fuhadShiblu_inputName   = document.querySelector('.fuhadShiblu_inputName');
+let fuhadShiblu_nameErr                     = document.querySelector('.fuhadShiblu_nameErr');
+let fuhadShiblu_passErr                     = document.querySelector('.fuhadShiblu_passErr');
+
+//========== Functions
+button.addEventListener('click' , (e)=>{
+    e.preventDefault()
+    if(fuhadShiblu_inputName.value == ''){
+        fuhadShiblu_nameErr.innerHTML = 'Please Enter your Username'
+    }else if(fuhadShiblu_inputPass.value == ''){
+        fuhadShiblu_passErr.innerHTML = 'Please Enter your Username'
+    }else{
+        fuhadShiblu_nameErr.innerHTML = ''
+        fuhadShiblu_inputName.value = ''
+        fuhadShiblu_passErr.innerHTML = ''
+        fuhadShiblu_inputPass.value = ''
+        console.log('Success')
+    }
+})
